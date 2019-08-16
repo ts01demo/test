1a2c561e36641b77c9fbf9726efda236ed6a0606
ts01demo <54092445+ts01demo@users.noreply.github.com>
2019-08-15 20:50:16 -0500

    Delete Create_Text_Docs.ps1

--- Create_Text_Docs.ps1
+++
@@ -1,9 +0,0 @@
-﻿for($i = 0; $i -lt 10; $i++)
-{
-    $Random= Get-Random -Maximum 2048
-    $Content=-join ((65..90) + (97..122) | Get-Random -Count $Random | % {[char]$_})
-    $FileName=-join ((65..90) + (97..122) | Get-Random -Count 10 | % {[char]$_})
-    $FileName = $FileName + ".txt"
-    $Content | Out-File -FilePath .\$FileName
-    
-}
